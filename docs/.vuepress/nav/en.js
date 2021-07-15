5584af83cb1a8fc40af1be3e20b961cdefdbc2ec
Zé Bateira <jose.bateira@protonmail.com>
2021-07-15 12:05:08 +0100

    feat: add navigation links
    Default main domain is staging.web3.storage. On launch we need to set
    the env var MAIN_DOMAIN to web3.storage so all the links are correct.

--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -1,1 +1,9 @@
-module.exports = []
+const mainDomain = process.env.MAIN_DOMAIN || 'https://staging.web3.storage';
+
+module.exports = [
+    { text: 'Home', link: mainDomain, target: '_self', rel: false },
+    { text: 'About', link: `${mainDomain}/about`, target: '_self', rel: false },
+    { text: 'Docs', link: '/' },
+    { text: 'Files', link: `${mainDomain}/files`, target: '_self', rel: false },
+    { text: 'Profile', link: `${mainDomain}/profile`, target: '_self', rel: false }
+]
